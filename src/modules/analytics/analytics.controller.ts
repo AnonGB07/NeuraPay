474cb69099884baadf2738a03c0db6dc2243b9d8
AnonGB07 <156324434+AnonGB07@users.noreply.github.com>
2025-05-21 19:18:39 -0400

    Create analytics.controller.ts

---
+++ src/modules/analytics/analytics.controller.ts
@@ -0,0 +1,15 @@
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { AnalyticsService } from './analytics.service';
+import { JwtAuthGuard } from '../../common/guards/jwt-auth.guard';
+import { User } from '../../common/decorators/user.decorator';
+
+@Controller('analytics')
+@UseGuards(JwtAuthGuard)
+export class AnalyticsController {
+  constructor(private readonly analyticsService: AnalyticsService) {}
+
+  @Get()
+  async getUserAnalytics(@User('id') userId: number) {
+    return this.analyticsService.getUserAnalytics(userId);
+  }
+}
