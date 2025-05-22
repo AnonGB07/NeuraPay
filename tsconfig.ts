df9d6459635ee69af2a3e3b2e419bf2f6e9496fc
AnonGB07 <156324434+AnonGB07@users.noreply.github.com>
2025-05-21 19:03:09 -0500

    Update tsconfig.ts

--- tsconfig.ts
+++ tsconfig.ts
@@ -20,14 +20,16 @@
 
 
 
+root@srv791876:~/neurapay-backend# npm cache clean --force
+npm WARN using --force Recommended protections disabled.
 root@srv791876:~/neurapay-backend# npm install
 npm ERR! code E404
-npm ERR! 404 Not Found - GET https://registry.npmjs.org/@nestjs-modules%2fredis - Not found
+npm ERR! 404 Not Found - GET https://registry.npmjs.org/@nestjs%2fredis - Not found
 npm ERR! 404 
-npm ERR! 404  '@nestjs-modules/redis@^2.0.0' is not in this registry.
+npm ERR! 404  '@nestjs/redis@^9.0.0' is not in this registry.
 npm ERR! 404 
 npm ERR! 404 Note that you can also install from a
 npm ERR! 404 tarball, folder, http url, or git url.
 
 npm ERR! A complete log of this run can be found in:
-npm ERR!     /root/.npm/_logs/2025-05-21T23_51_43_520Z-debug-0.log
+npm ERR!     /root/.npm/_logs/2025-05-22T00_01_28_219Z-debug-0.log
